e32629df3915c49c4887ad0da1ce99c40d3f6a7f
Pristine <prisvsyo@gmail.com>
2024-08-21 12:30:36 -0700

    completed useDispatch useSelector practice

--- Mod 5 Week 3 (29)/3-day-react-redux-useDispatch-useSelector-part-time/src/components/ArticleList/ArticleList.jsx
+++ Mod 5 Week 3 (29)/3-day-react-redux-useDispatch-useSelector-part-time/src/components/ArticleList/ArticleList.jsx
@@ -1,14 +1,28 @@
+import { useEffect } from 'react';
+import { NavLink} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadArticles } from '../../store/articleReducer'
 
 const ArticleList = () => {
+
+  const dispatch = useDispatch();
+  const articles = useSelector(state => state.articleState.entries
+  );
+
+  useEffect(() => {
+    dispatch(loadArticles())
+  }, [dispatch])
+
+
   return (
     <div>
       <h1>Article List</h1>
       <ol>
-        <li>Gilligan&apos;s Island. Is it true?</li>
-        <li>A Baseball Moment</li>
-        <li>Poke Moment</li>
-        <li>Cool Cats</li>
-        <li>Why Am I At Home</li>
+        {articles.map(({id, title}) => (
+          <li key={id}>
+            <NavLink to={`${id}`}>{title}</NavLink>
+          </li>
+        ))}
       </ol>
     </div>
   );
