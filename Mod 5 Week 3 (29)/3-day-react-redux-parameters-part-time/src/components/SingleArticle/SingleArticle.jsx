07e0655e21c27bc04f3a4aa0b21271ad1468f658
Pristine Shin <prisvsyo@gmail.com>
2024-08-21 18:00:20 -0700

    pausing at end of parameters

--- Mod 5 Week 3 (29)/3-day-react-redux-parameters-part-time/src/components/SingleArticle/SingleArticle.jsx
+++ Mod 5 Week 3 (29)/3-day-react-redux-parameters-part-time/src/components/SingleArticle/SingleArticle.jsx
@@ -1,18 +1,26 @@
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
 import './SingleArticle.css';
 
 const SingleArticle = () => {
+
+  let { id } = useParams();
+
+  const articles = useSelector(state=>state.articleState.entries);
+
+  const selectedArticle = articles.find(article => article.id === id)
+
+  // console.log(selectedArticle.id)
   return (
     <div className='singleArticle'>
-      <h1>Why Am I At Home</h1>
+      <h1>{`${selectedArticle.title}`}</h1>
       <img
-        src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Frobcain%2Ffiles%2F2017%2F10%2FKevin-Home-Alone.jpg'
+        src={`${selectedArticle.imageUrl}`}
         alt='home'
       />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex esse
-        laboriosam officia accusantium veritatis fugiat exercitationem vero
-        autem nihil aliquid ullam recusandae, quis odit odio voluptates
-        explicabo nobis! Consequuntur, aliquam?
+        {`${selectedArticle.imageUrl}`}
       </p>
     </div>
   );
