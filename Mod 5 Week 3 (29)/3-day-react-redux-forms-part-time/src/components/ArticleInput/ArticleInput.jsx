748a55fdb4bdb7f7f9e2d182fa25abdb7478d017
Pristine <prisvsyo@gmail.com>
2024-08-22 14:23:04 -0700

    pausing at forms

--- Mod 5 Week 3 (29)/3-day-react-redux-forms-part-time/src/components/ArticleInput/ArticleInput.jsx
+++ Mod 5 Week 3 (29)/3-day-react-redux-forms-part-time/src/components/ArticleInput/ArticleInput.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
+import { addArticle } from '../../store/articleReducer';
 import './ArticleInput.css';
 
 const ArticleInput = () => {
+  const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [imageUrl, setImageUrl] = useState('');
+
+  const newArticle = {
+    id: nanoid(),
+    title,
+    imageUrl,
+    body
+  }
+
+  console.log(newArticle);
+
+  useEffect(() => {
+    dispatch(addArticle(newArticle));
+  }, [dispatch]);
+
+  // if (!newArticle) {
+  //   return (
+  //     <h1>Loading...</h1>
+  //   )
+  // }
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- Mod 5 Week 3 (29)/3-day-react-redux-forms-part-time/src/store/articleReducer.js
+++ Mod 5 Week 3 (29)/3-day-react-redux-forms-part-time/src/store/articleReducer.js
@@ -1,6 +1,7 @@
 import articles from '../data/data.json';
 
 const LOAD_ARTICLES = 'article/loadArticles';
+const ADD_ARTICLE = 'article/addArticle';
 
 export const loadArticles = () => {
   return {
@@ -9,12 +10,23 @@ export const loadArticles = () => {
   };
 };
 
+export const addArticle = (article) => {
+  return {
+    type: ADD_ARTICLE,
+    article
+  }
+}
+
 const initialState = { entries: [], isLoading: true };
 
 const articleReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_ARTICLES:
       return { ...state, entries: [...action.articles] };
+    case ADD_ARTICLE:
+      const newState = {...state}; // shallow copy
+      newState.entries = [...newState.entries, action.article]; //adds article and makes a new array shallow copied
+      return newState
     default:
       return state;
   }
