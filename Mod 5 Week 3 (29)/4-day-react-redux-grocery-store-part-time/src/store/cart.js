8f1be7097d357794f5e4c3ec3a380f43d9875a90
Pristine <prisvsyo@gmail.com>
2024-08-23 17:14:02 -0700

    middle of phase 3

---
+++ Mod 5 Week 3 (29)/4-day-react-redux-grocery-store-part-time/src/store/cart.js
@@ -0,0 +1,22 @@
+
+const ADD_TO_CART = 'cart/addToCart';
+
+const addToCart = (produceId) => ({
+  type: ADD_TO_CART,
+  produceId
+})
+
+const cartReducer = (state = {}, action) => {
+  switch(action.type) {
+    case ADD_TO_CART:
+      return { ...state, [action.produceId]: {
+        id: action.produceId,
+        count: 1
+      }
+    }
+    default:
+      return state;
+  }
+}
+
+export default cartReducer;
