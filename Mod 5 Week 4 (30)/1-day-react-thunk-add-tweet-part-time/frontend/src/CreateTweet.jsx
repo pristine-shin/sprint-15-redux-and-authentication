2b336262867309d45f06c2427ea8ad9433f04231
Pristine Shin <prisvsyo@gmail.com>
2024-08-28 16:10:53 -0700

    completed tweet practice

--- Mod 5 Week 4 (30)/1-day-react-thunk-add-tweet-part-time/frontend/src/CreateTweet.jsx
+++ Mod 5 Week 4 (30)/1-day-react-thunk-add-tweet-part-time/frontend/src/CreateTweet.jsx
@@ -10,8 +10,10 @@ export default function CreateTweet() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const newTweet = { tweet }
-    
+    const newTweet = {
+      message: tweet
+    };
+
     if (!newTweet) {
       window.alert('Please enter a tweet');
     } else {
@@ -22,6 +24,7 @@ export default function CreateTweet() {
 
   return (
     <form onSubmit={handleSubmit}>
+      <h1>Post your tweet!</h1>
       <input type="text" value={tweet} onChange={e => setTweet(e.target.value)}/>
       <button>Submit</button>
     </form>
