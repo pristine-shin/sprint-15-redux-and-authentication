e72720af3a8f6c157fd69a7735db2974a13dd03e
Pristine <prisvsyo@gmail.com>
2024-08-29 13:50:29 -0700

    completed phase 5 load pokemon items

--- Mod 5 Week 4 (30)/2-day-react-thunk-pokedex-part-time/frontend/src/components/PokemonItems.jsx
+++ Mod 5 Week 4 (30)/2-day-react-thunk-pokedex-part-time/frontend/src/components/PokemonItems.jsx
@@ -1,10 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getItemsByPokemonId } from '../store/items';
+
 
 const PokemonItems = ({ pokemon, setEditItemId }) => {
+  const { pokemonId } = useParams();
+  const dispatch = useDispatch();
   const items = useSelector((state) => {
     if (!pokemon.items) return null;
     return pokemon.items.map(itemId => state.items[itemId]);
   });
+
+  useEffect(() => {
+    dispatch(getItemsByPokemonId(pokemonId))
+  }, [pokemonId])
 
   if (!items) {
     return null;
--- Mod 5 Week 4 (30)/2-day-react-thunk-pokedex-part-time/frontend/src/store/items.js
+++ Mod 5 Week 4 (30)/2-day-react-thunk-pokedex-part-time/frontend/src/store/items.js
@@ -25,6 +25,15 @@ const remove = (itemId, pokemonId) => ({
   pokemonId
 });
 
+export const getItemsByPokemonId = (pokemonId) => async dispatch => {
+  const response = await fetch(`/api/pokemon/${pokemonId}/items`);
+
+  if (response.ok) {
+    const itemsByPokemonId = await response.json();
+    dispatch(load(itemsByPokemonId, pokemonId));
+  }
+}
+
 const initialState = {};
 
 const itemsReducer = (state = initialState, action) => {
@@ -45,7 +54,7 @@ const itemsReducer = (state = initialState, action) => {
       return newState;
     }
     case ADD_ITEM:
-    case UPDATE_ITEM: 
+    case UPDATE_ITEM:
       return {
         ...state,
         [action.item.id]: action.item
