cf838f65324b5a6e791820d1f14fc0349d117378
Pristine <prisvsyo@gmail.com>
2024-08-30 14:30:37 -0700

    in the middle of phase 1 for authme

---
+++ Mod 5 Week 4 (30)/3-day-react-redux-authenticate-me-part-time/frontend/src/store/session.js
@@ -0,0 +1,54 @@
+import { csrfFetch } from './csrf';
+
+// constant to avoid debugging typos
+const ADD_SESSION = 'session/add_session';
+const REMOVE_SESSION = 'session/remove_session';
+
+//regular action creator
+const addSession = (user) => {
+  return {
+    type: ADD_SESSION,
+    user
+  };
+};
+
+const removeSession = () => {
+  return {
+    type: REMOVE_SESSION,
+  }
+}
+
+// thunk action creator
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch('/api/session', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      credential,
+      password
+    }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(addSession(data.user));
+    return response;
+  }
+}
+
+// reducer
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_SESSION: {
+      const newState = {...state, user: action.user};
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
